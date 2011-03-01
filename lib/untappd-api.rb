aa9334f2620168ccb0de3596d797ff7978586bd3
Jeff Smith <smith734@msu.edu>
2011-02-28 22:12:43 -0500

    Pointed to api v3 and added new methods.  Removed Hashie dependency, all methods now return a normal Hash representing the response.

--- lib/untappd-api.rb
+++ lib/untappd-api.rb
@@ -1,18 +1,27 @@
 require 'httparty'
-require 'hashie'
 require 'digest/md5'
-
-Hash.send :include, Hashie::HashExtensions
 
 module Untappd
   class Base
     include HTTParty
-    BASE_URI = 'http://api.untappd.com/v2'
+    BASE_URI = 'http://api.untappd.com/v3'
     base_uri BASE_URI
 
-    def initialize(user, pass, key)
-      @auth = { :username => user, :password => Digest::MD5.hexdigest(pass) }
+    def initialize(key, user = nil, pass = nil)
+      @auth = {}
+      if user && pass
+        @auth = { :username => user, :password => Digest::MD5.hexdigest(pass) }
+      end
       @key = key
+    end
+
+    # Get the beer's feed
+    # Params: 
+    #   :bid => Your API Key provided when you are approved
+    #   :since => The numeric ID of the last recent check-in. (Optional)
+    #   :offset => The offset that you like the dataset to begin with. (Optional)
+    def beer_feed(params={})
+      get('beer_checkins', params)
     end
 
     # Get extended information about a beer.
@@ -29,11 +38,20 @@ module Untappd
       get('beer_search', params)
     end
 
+    # Get the brewery's feed
+    # Params: 
+    #   :brewery_id => Your API Key provided when you are approved
+    #   :since => The numeric ID of the last recent check-in. (Optional)
+    #   :offset => The offset that you like the dataset to begin with. (Optional)
+    def brewery_feed(params={})
+      get('brewery_checkins', params)
+    end
+
     # Get extended details for a particular checkin, 
     # which includes location, comments and toasts.
     # Params:
     #   :id => The numeric ID of the check-in.
-    def checkin_details(params={})
+    def checkin_info(params={})
       get('details', params)
     end
 
@@ -55,12 +73,22 @@ module Untappd
       get('thepub', params)
     end
 
+    # Get currently trending beers.
+    # Params:
+    #   :type => "all", "macro", "micro", "local".  "all" set to default. (Optional)
+    #   :limit => The number of records that will return (max 10). (Optional)
+    #   :age => "daily", "weekly", "monthly". (Optional)
+    #   :geolat =>  The numeric Latitude to filter the public feed. (Optional, required for type "local")
+    #   :geolng =>  The numeric Longitude to filter the public feed. (Optional, required for type "local")
+    def trending(params={})
+      get('trending', params)
+    end
+
     # Get the user information for a selected user.
     # Params:
     #   :user => The username of the person who you wish to obtain the user information. (Optional)
-    def user(params={})
-      result = get('user', params)
-      result.user unless result.nil?
+    def user_info(params={})
+      get('user', params)
     end
 
     # Get a list of the user's badges.
@@ -104,17 +132,35 @@ module Untappd
       get('wish_list', params)
     end
 
+    # Get the venue's feed
+    # Params: 
+    #   :venue_id => Your API Key provided when you are approved
+    #   :since => The numeric ID of the last recent check-in. (Optional)
+    #   :offset => The offset that you like the dataset to begin with. (Optional)
+    def venue_feed(params={})
+      get('venue_checkins', params)
+    end
+
+    # Get extended information about a venue.
+    # Params:
+    #   :venue_id => The numeric beer ID of the beer you wish to look up.
+    def venue_info(params={})
+      get('venue_info', params)
+    end
+
     private
 
     def get(method, params={})
       path = "/#{method}?key=#{@key}"
       params.each { |k,v| path += "&#{k.to_s}=#{v.to_s}" }
 
-      options = {:basic_auth => @auth}
+      options = {}
+      options.merge({:basic_auth => @auth}) unless @auth.empty?
+
       response = self.class.get(URI.escape(path), options)
       raise_errors(response)
 
-      response.parsed_response.to_mash.results 
+      response.parsed_response
     end
 
     def raise_errors(response)
